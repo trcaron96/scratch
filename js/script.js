ff6309e1b03f2e2bc8635cb01960fccc52f77208
trcaron96 <64705344+trcaron96@users.noreply.github.com>
2020-05-08 13:53:37 -0400

    added script fonts

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var bigFont = function () {
+	document.getElementById('demo').style.fontSize='35px'
+}
+
+var smallFont = function () {
+	document.getElementById('demo').style.fontSize='15px'
+}
+
+var hide = function () {
+	document.getElementById('demo').style.display='none'
+}
+
+var show = function () {
+	document.getElementById('demo').style.display='block'
+}
